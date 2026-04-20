1c6cebe37e6d3757b52e3e652f4e29c8f71aa618
Vhackenstance <hackenstance@proton.me>
2026-04-20 11:14:39 -0500

    Reduild

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -5,7 +5,7 @@ if __name__ == "__packet_sniffer__":
     print("packet_sniffer is being called")
 
 def sniff(interface):
-    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet, filter="udp")
 
 def process_sniffed_packet(packet):
     print(packet)
