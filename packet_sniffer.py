cb2854992241da2d4152f4ade310c01fa4545415
Vhackenstance <hackenstance@proton.me>
2026-05-20 11:57:57 -0500

    Basic sniffer

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,2 +1,14 @@
 #/usr/bin/env python
 # Rebuild
+import scapy.all as scapy
+from scapy.layers import http
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        print(packet)
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+sniff("eth0")
+
