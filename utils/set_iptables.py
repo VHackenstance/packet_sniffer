fec96b017a10ba06952e612532160482f9421a9f
Vhackenstance <hackenstance@proton.me>
2026-06-18 09:19:03 -0500

    Set the IP Tables

--- utils/set_iptables.py
+++ utils/set_iptables.py
@@ -1,27 +1,5 @@
 #/usr/bin/env python
 import subprocess
-try:
-	from subprocess import DEVNULL
-except ImportError:
-	import os
-	# pylint: disable-msg=C0103
-	DEVNULL = open(os.devnull, 'wb')
-
-def enable_port_forwarding():
-	print("[+] Enable Port Forwarding...")
-	subprocess.call(
-		'echo 1 | sudo tee "/proc/sys/net/ipv4/ip_forward"',
-		shell=True,
-		stdout=DEVNULL,
-	)
-	# Check to make sure this worked
-	with open('/proc/sys/net/ipv4/ip_forward', 'r') as f:
-		value = f.read().strip()
-		if value == "1":
-			print("[+] Port Forwarding enabled.")
-		else:
-			print("[-] Something went wrong.  Manually enable port forwarding.")
-			exit()
 
 def set_iptables():
 	number = None
@@ -43,17 +21,4 @@ def set_iptables():
 	# return the number to set in the queue call
 	return number
 
-print("[+] Setting up environment...")
-# 1. Enable Port Forwarding.
-enable_port_forwarding()
-# 2. Set IP Tables
 set_iptables()
-
-	# 4. Call SSLStrip using Bettercap
-
-# B. On EXIT
-	# 1. Disable port forwarding
-	# 2. Flush iptables
-	# 3. Shutdown ARP Spoof
-	# 4. Shutdown Bettercap
-	# 5. Say bye bye and laters
